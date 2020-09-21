2635deb9076f168cc41672b4c01aafe7dd8c4996
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-09-21 12:22:08 +0200

    Added bomb sprite

--- lib/components/bomb.dart
+++ lib/components/bomb.dart
@@ -6,27 +6,22 @@ import 'package:talpasplat3/game_controller.dart';
 
 class Bomb {
   final GameController gameController;
-  final int maxJumpInterval = 2000;
-  final int minJumpInterval = 100;
-  final int intervalChange = 20;
-  int currentInterval;
-  int nextJump;
 
   Rect bombRect;
   Sprite bombSprite;
-  final double size = 80.0;
+  final double size = 50.0;
 
   Random rand;
 
   Bomb(this.gameController) {
     rand = Random();
+    bombSprite = Sprite('bomb.png');
     bombRect = Rect.fromLTWH(gameController.screenSize.width / 2 - size / 2,
-        gameController.screenSize.height / 2 - size / 2, size, size);
+        gameController.screenSize.height / 2 - size / 2, size, size * 1.2);
   }
 
   void render(Canvas c) {
-    Paint bombPaint = Paint()..color = Color(0xFFFAFAFA);
-    c.drawRect(bombRect, bombPaint);
+    bombSprite.renderRect(c, bombRect);
   }
 
   void update(double t) {}
