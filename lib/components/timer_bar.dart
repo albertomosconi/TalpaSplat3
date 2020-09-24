479611173dd0b9843625767e46dba367390fde33
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-09-24 16:54:47 +0200

    Added shadow to timer bar

--- lib/components/timer_bar.dart
+++ lib/components/timer_bar.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
 import 'package:talpasplat3/game_controller.dart';
 
 class TimerBar {
@@ -9,14 +10,17 @@ class TimerBar {
   double barWidth;
   double barHeight;
   Paint currentTimeBarColor;
+  Path shadowPath;
 
   TimerBar(this.gameController) {
     barWidth = gameController.screenSize.width *
         (gameController.currentTime / gameController.gameDuration);
     barHeight = 15.0;
 
-    //timerBarRect = Rect.fromLTWH(
-    //    0, gameController.screenSize.height - barHeight, barWidth, barHeight);
+    shadowPath = Path();
+    shadowPath.lineTo(barWidth, 0.0);
+    shadowPath.lineTo(barWidth, barHeight);
+    shadowPath.lineTo(0.0, barHeight);
 
     currentTimeBarRect = Rect.fromLTWH(
         0,
@@ -31,12 +35,26 @@ class TimerBar {
     } else {
       currentTimeBarColor = Paint()..color = Color(0xFFFFFFFF);
     }
+
+    c.drawShadow(shadowPath, Colors.black, 8, false);
+    c.drawShadow(shadowPath, Colors.black, 8, false);
+    c.drawShadow(shadowPath, Colors.black, 8, false);
+    c.drawShadow(shadowPath, Colors.black, 8, false);
     c.drawRect(currentTimeBarRect, currentTimeBarColor);
   }
 
   void update(double t) {
     barWidth = gameController.screenSize.width *
         (1 - gameController.currentTime / gameController.gameDuration);
+
+    shadowPath = Path();
+    shadowPath.lineTo(barWidth, 0.0);
+    shadowPath.lineTo(barWidth, barHeight);
+    shadowPath.lineTo(0.0, barHeight);
+
+    shadowPath = shadowPath.shift(Offset(
+        (gameController.screenSize.width - barWidth) / 2,
+        gameController.screenSize.height - barHeight - 12.0));
 
     currentTimeBarRect = Rect.fromLTWH(
         (gameController.screenSize.width - barWidth) / 2,
