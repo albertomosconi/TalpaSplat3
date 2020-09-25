291e2d9fc37a40e39e208ff114046141797b063b
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-09-25 17:08:00 +0200

    better homescreen + help and credits icons

--- lib/views/home-view.dart
+++ lib/views/home-view.dart
@@ -13,26 +13,45 @@ class HomeView {
   Sprite titleSprite;
   Rect startButtonRect;
   Sprite startButtonSprite;
+  Rect helpIconRect;
+  Sprite helpIconSprite;
+  Rect creditsIconRect;
+  Sprite creditsIconSprite;
 
   HomeView(this.gameController) {
     background = Background(gameController, 'background.png');
 
     titleRect = Rect.fromLTWH(
       gameController.tileSize,
-      (gameController.screenSize.height / 2) - (gameController.tileSize * 4),
+      (gameController.screenSize.height / 2) - (gameController.tileSize * 6),
       gameController.tileSize * 7,
       gameController.tileSize * 4,
     );
     titleSprite = Sprite('ui/title.png');
 
     startButtonRect = Rect.fromLTWH(
-      gameController.tileSize * 1.5,
-      (gameController.screenSize.height * .75) -
-          (gameController.tileSize * 1.5),
-      gameController.tileSize * 6,
-      gameController.tileSize * 3,
+      gameController.tileSize * 2.5,
+      (gameController.screenSize.height * .75) - (gameController.tileSize),
+      gameController.tileSize * 4,
+      gameController.tileSize * 2,
     );
     startButtonSprite = Sprite('ui/start-button.png');
+
+    helpIconRect = Rect.fromLTWH(
+      gameController.tileSize * .25,
+      gameController.screenSize.height - (gameController.tileSize * 1.25),
+      gameController.tileSize,
+      gameController.tileSize,
+    );
+    helpIconSprite = Sprite('ui/icon-help.png');
+
+    creditsIconRect = Rect.fromLTWH(
+      gameController.screenSize.width - (gameController.tileSize * 1.25),
+      gameController.screenSize.height - (gameController.tileSize * 1.25),
+      gameController.tileSize,
+      gameController.tileSize,
+    );
+    creditsIconSprite = Sprite('ui/icon-credits.png');
   }
 
   void render(Canvas c) {
@@ -40,6 +59,8 @@ class HomeView {
 
     titleSprite.renderRect(c, titleRect);
     startButtonSprite.renderRect(c, startButtonRect);
+    helpIconSprite.renderRect(c, helpIconRect);
+    creditsIconSprite.renderRect(c, creditsIconRect);
   }
 
   void update(double t) {}
